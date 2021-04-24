b7a068a0c600ad5f07996cffd98ec9cf6c212a9f
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-04-24 16:59:52 +0300

    Update script.js

--- Homework9/script.js
+++ Homework9/script.js
@@ -84,8 +84,5 @@ function mapBuilder(keys, values){
 let keys = [1, 2, 3, 4];
 let values = ["div", "span", "b", "i"];
 let map=mapBuilder(keys, values);
-//let map = mapBuilder(keys, values);
-//console.log(map.size); // 4
-//	console.log(map.get(2)); // "span"
-//console.log(map)*/
+
 
