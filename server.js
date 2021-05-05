3842f8e9bbd821ef1f7d184772388b505075b705
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-05-05 16:03:18 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,60 +1,35 @@
+//task1
 const http = require("http");
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});  
-if (request.method=='GET')
-{
-    response.end('get ')
-}
-else if (request.method=='POST'){
-    response.end(' post ')
-}
-else{
-    response.end('not')
-}
-//response.write(sendServer());
-})
-.listen(8000);
-console.log('Server running at http://127.0.0.1:8000/');
+const os = require("os");
+response.write('OS type : ' + os.type());
+response.write(' Current user name: ' + os.userInfo().username);
+let uptime=os.uptime();
+response.write(' System work time : ' +(uptime/60) +' minutes');
+const path=require('path');
+pat='C:\nodejs\server.js'
+response.write(' Current work directory: ' + path.dirname(pat));
+response.write(' Server fail name: ' + path.basename(pat))
+console.log('Server running at http://127.0.0.1:5000/');
+response.end('Testing NodeJS server\n');
+}).listen(5000);
 
-/*const { response } = require("express");
-const express = require("express");
-const app = express();
- 
-app.use(function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
- //const modul=require('.//modul')
- //response.write(`${module}`)
-response.end('Testing NodeJS server\n').listen(5000);
 
+
+
+
+
+//task2
+const http = require("http");
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+const personalmodule=require('.//personalmodule');
+response.write(`Date of request: ${personalmodule.date}`)
+const os = require("os");
+  let name=os.userInfo().username;
+  response.write(personalmodule.getMesssage(name));
+  response.end('Testing NodeJS server\n');
+}).listen(5000);
 console.log('Server running at http://127.0.0.1:5000/');
 
-
-let express = require('express'),
-    app = express(),
-    path = require("path");
-let bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-app.use('/handler', bodyParser.urlencoded({
-    extended: true
-}));
-
-app.post('/handler', function(req, res, next) {
-    // это обработчик на сервере, после отправки формы url заканчивается на "/handler"
-    console.log(req.body);
-    console.log("Cработал!");
-    res.type('text/plain');
-    res.json(req.body);
-    res.end();
-});
-
-// ------ запустить сервер -------
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-console.log("Запущен сервер, порт 3000");
-app.listen(8000);*/
