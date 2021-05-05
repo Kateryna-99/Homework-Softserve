909ed9febe7f84222bb6415e0fd84c414a91a31f
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-05-05 12:24:14 +0300

    Update script.js

--- Project/script.js
+++ Project/script.js
@@ -1,6 +1,11 @@
 
 //подія, яка реєструція при відкр.сторінки і слухає статус мережі
 //при онлайн - перевіряє локальне сховище, виводить дані на сторінку (якщо є)б
+
+//const { emit } = require("node:cluster");
+
+//const { url } = require("node:inspector");
+
 //потім видаляє їх з localStorage
 document.addEventListener('DOMContentLoaded',function(){
         if (navigator.onLine){
@@ -49,7 +54,9 @@ array[i]=inp[i].value;
    localStorage.setItem("array",JSON.stringify(array));                             //створюємо масив з інпутів
 }
 
+//створення сервера
 //відправляємо дані на сервер
+/*
 let form=document.forms.user;
 function sendServer(){
     //let form=document.forms.form;
@@ -65,9 +72,75 @@ function sendServer(){
 }
     request.send(formData);
 }
+*
+function sendServer(){
+$(function () {
+    $("#ident").click(function (event) {
+      event.preventDefault();
+      $.post("/handler", $(this).serialize(),
+      function(data){
+          $('#answer')/append(data);
+      });
+      $('#formOut').append(trHTML);
+    })
+  })
+}
 
+function sendServer(){
+let data=$('#ident').serialize();
+$.ajax({
+    type:"POST",
+    url: "http://127.0.0.1:8000/",
+    data: data,
+    headers:{'Content-type': 'application/json; charset=UTF-8',},
+    mode: 'cors',
+    success:function(msg){
+        alert(msg);
+    }
 
+})}
+*/
+  
+function sendServer(){
+    //ident.onclick=
+async (e)=>{
+    e.preventDefault();
+    let response=await fetch('http://127.0.0.1:8000',
+    {
+        mode: 'cors',
+        method: 'POST',
+        body: new FormData(ident)
+    });
+    let result=await response.json();
+    alert(result.message);
+};
+}
+//випадаючі списки для вибору потрібноі спеціальності в ІТ
+const specialArr={
+    'tech':['Back-end developer', 'Front-end developer', 'Nest Engineer/QA', 'Full stack'],
+    'non_tech':['Rpoject manager', 'HR-manager', 'Prodact manager', 'Recruter', 'PR-manager', 'Tech Lead', 'Event manager'],
+}
+let prime_select=document.getElementById('direction');
+prime_select.addEventListener('change', func);
+prime_select.addEventListener('change', toParagraph);
 
+let special_select=document.getElementById('specialties');
+special_select.addEventListener('change',toParagraph);
+function func(){
+    special_select.innerHTML='';
+    let selected_direction=prime_select.value;
+    for (let i=0;i<specialArr[selected_direction].length; i++){
+        let new_option=document.createElement('option');
+        new_option.innerHTML=specialArr[selected_direction][i];
+        special_select.appendChild(new_option)
+    }
+}
+let parag=document.querySelector('p');
+function toParagraph(){
+    parag.innerHTML='';
+    parag.innerHTML=prime_select.options[prime_select.selectedIndex].text+','
+    +special_select.options[special_select.selectedIndex].text
+}
 
 
 
